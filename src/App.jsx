161a53ea611a3da1ceb2b73ceec8ec48f4f83210
tanmaysayare <tanmaysayare12345@gmail.com>
2024-05-29 23:15:10 +0530

    Dot function addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+
+
+
+const App = () => {
+  const [allClicks, setAll] = useState([]);
+  const [dot , setDot] = useState(0)
+
+  const handleDot = () => {
+    setAll(allClicks.concat('*'))
+    setDot(dot + 1)
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Display allClicks={allClicks}/>
+      <Button onClick={handleDot} text='Dot' />
+      
+    </div>
+  )
+}
+
+
+const Display = ({allClicks}) => {
+  return (
+    <div>
+      <p><h1>Cricket Score Board</h1></p>
+      <p>Over: {allClicks.join("  ")}</p>
+      <h2>Total Score : </h2>
+    </div>
+  )  
+}
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
   )
 }
 
