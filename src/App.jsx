3df7be1fcdb9611b179cd3831f7d3ef73b16c53e
tanmaysayare <tanmaysayare12345@gmail.com>
2024-05-29 23:45:41 +0530

    Over completed disable buttons added

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,69 @@
 import { useState } from 'react'
 import './App.css'
 
-
-
-
-
-
 const App = () => {
   const [allClicks, setAll] = useState([]);
   const [dot , setDot] = useState(0)
   const [wicket , setWicket] = useState(0)
   const [run , setRun] = useState(0)
-  const [wide , setWide] = useState(0)
   const [balls , setBall] = useState(0)
+  const [isOverCompleted, setIsOverCompleted] = useState(false);
+
 
 
   const handleDot = () => {
-    setAll(allClicks.concat('*'))
-    setDot(dot + 1)
-    setBall(balls + 1)
-  }
-
+    if (balls < 6) {
+      setAll(allClicks.concat('*'));
+      setDot(dot + 1);
+      setBall(balls + 1);
+    }
+    if (balls + 1 === 6) {
+      setIsOverCompleted(true);
+    }
+  };
+  
   const handleWicket = () => {
-    setAll(allClicks.concat('W'))
-    setWicket(wicket + 1)
-    setBall(balls + 1)
-  }
-
+    if (balls < 6) {
+      setAll(allClicks.concat('W'));
+      setWicket(wicket + 1);
+      setBall(balls + 1);
+    }
+    if (balls + 1 === 6) {
+      setIsOverCompleted(true);
+    }
+  };
+  
   const handleRun = (runs) => {
-    setAll(allClicks.concat(runs))
-    const newRun = run + runs
-    setRun(newRun)
-    setBall(balls + 1)
-  }
-
+    if (balls < 6) {
+      setAll(allClicks.concat(runs));
+      setRun(run + runs);
+      setBall(balls + 1);
+    }
+    if (balls + 1 === 6) {
+      setIsOverCompleted(true);
+    }
+  };
+  
   const handleWide = () => {
-    setAll(allClicks.concat('Wd'))
-    setRun(run + 1)
-  }
+    if (balls < 6) {
+      setAll(allClicks.concat('Wd'));
+      setRun(run + 1); // Stop increment balls for wide
+    }
+  };
+  
 
   return (
     <div>
       <Display allClicks={allClicks} runs={run} ball={balls}/>
-      <Button onClick={handleDot} text='Dot' />
-      <Button onClick={handleWicket} text='Wicket' />
-      <Button onClick={handleWide} text='Wd' />
+      <Button onClick={handleDot} text='Dot' disabled={isOverCompleted}/>
+      <Button onClick={handleWicket} text='Wicket' disabled={isOverCompleted}/>
+      <Button onClick={handleWide} text='Wd' disabled={isOverCompleted}/>
       <p>
-      <Button onClick={() => handleRun(1)} text='1' />
-      <Button onClick={() => handleRun(2)} text='2' />
-      <Button onClick={() => handleRun(3)} text='3' />
-      <Button onClick={() => handleRun(4)} text='4' />
-      <Button onClick={() => handleRun(6)} text='6' />
+      <Button onClick={() => handleRun(1)} text='1' disabled={isOverCompleted}/>
+      <Button onClick={() => handleRun(2)} text='2' disabled={isOverCompleted}/>
+      <Button onClick={() => handleRun(3)} text='3' disabled={isOverCompleted}/>
+      <Button onClick={() => handleRun(4)} text='4' disabled={isOverCompleted}/>
+      <Button onClick={() => handleRun(6)} text='6' disabled={isOverCompleted}/>
       </p>
     </div>
   )
@@ -83,9 +96,9 @@ const History = ({ allClicks }) => {
   return <div>Over: {allClicks.join("  ")}</div>;
 };
 
-const Button = ({ onClick, text }) => {
+const Button = ({ onClick, text, disabled }) => {
   return (
-    <button onClick={onClick}>
+    <button onClick={onClick} disabled={disabled}>
       {text}
     </button>
   )
